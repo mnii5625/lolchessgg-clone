ab0b2aba7ae9871a5d50a1996cc06cd212a0abfd
mmingi <alstmf77@naver.com>
2022-04-15 02:44:30 +0900

    Main Top Layout

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,11 +1,39 @@
 import type { NextPage } from 'next'
+import Image from 'next/image';
+
 import Header from '../components/Header'
+import styles from '../styles/Main.module.css';
+import bg_top from '../public/images/bg-top.jpg';
+import logo from '../public/images/lolchessgg-logo-bkbg.png';
+import banner_naver from '../public/images/banner-naver-search-20211116.png';
+
+import SearchBox from '../components/SearchBox';
+import MetaDeck from '../components/MetaDeck';
 
 
 const Home: NextPage = () => {
   return (
     <div>
       <Header/>
+      <div className={styles.main_top_box}>
+        <Image 
+          src={logo} 
+          width = {238}
+          height = {50}
+          alt='Logo'
+        />
+        <SearchBox name="main"/>
+        <Image 
+          src={banner_naver} 
+          width = {232}
+          height = {79}
+          alt='BannerNaver'
+        />
+      </div>
+      <div className={styles.main_container}>
+        <MetaDeck/>
+      </div>
+      
     </div>
   )
 }
